a18b191c00b88219ae6b7cdb193cc1421dec69c0
ChrisKirbyDev <chriskirbydev@gmail.com>
2020-04-11 19:14:59 +1200

    add array, install ejs, date and days of week working

--- app.js
+++ app.js
@@ -5,17 +5,38 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/")
-function(req, res) {
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+
   var today = new Date();
 
-  if (today.getDay() === 6 || today.getDay() === 0) {
-    res.send("Yay it's the weekend!");
-  } else {
-    res.send("Boo! I have to work!");
-  }
+var options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long"
+};
+
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", {kindOfDay: day, newListItems: items});
 
 });
+
+app.post("/", function(req, res){
+  var item = req.body.newItem
+
+  items.push(item);
+
+  res.redirect("/");
+
+});
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
